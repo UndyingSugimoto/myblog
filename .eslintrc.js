3520b5a2973d45c0c21fcb124b6d3b7c8d943193
杉元 康治 <kouji36000@yahoo.co.jp>
2020-05-30 22:55:14 +0900

    chore :add settings

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "stylelint-config-recommended",
+        "stylelint-config-styled-components",
+        'prettier',
+        'prettier/@typescript-eslint',
+    ],
+    "processors": [
+        "stylelint-processor-styled-components"
+      ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+        "prettier",
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        'prefer-template': [
+            'error'
+        ],
+    }
+};
