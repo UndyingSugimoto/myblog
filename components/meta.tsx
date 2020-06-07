6aebc36f2e13ccc82640be81e3653ec2f2e14033
Koji Sugimoto <kouji36000@yahoo.co.jp>
2020-06-07 17:18:14 +0900

    fix: change favicon.

--- components/meta.tsx
+++ components/meta.tsx
@@ -23,11 +23,6 @@ const Meta: React.FC = () => {
         href="/favicon/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
